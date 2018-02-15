8f272ea3bf881c745b18533f927054a40f73ba03
Johann150 <johann.galle@protonmail.com>
2018-02-15 19:22:12 +0100

    memory initalisation

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cassert>
+#include <vector>
 #include "cpu.h"
 
 int main() {
     cpu *c = (cpu*)malloc(sizeof(cpu));
     cpu_init(c);
-    byte *memory = {0x01, 3, 0x02, 4, 0x16, 0x2E};
-    c->mem = memory;
+    std::vector<byte> memory_vec{0x01, 0, 0, 0, 3, 0x02, 0, 0, 0, 4, 0x16, 0x2E};
+    c->mem = (byte*)malloc(memory_vec.size()*sizeof(byte));
+    std::copy(memory_vec.begin(),memory_vec.end(),c->mem);
 
     run(c);
 
