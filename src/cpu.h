49b31aafb5b19452ff8419271e3099be64a00768
Johann150 <johann.galle@protonmail.com>
2018-02-15 20:37:55 +0100

    got a little endian problem here
    pun intended

--- src/cpu.h
+++ src/cpu.h
@@ -17,6 +17,14 @@ struct cpu {
     byte *mem;
 };
 
+i64 endian_swap(i64 arg0){
+	uint64_t x = arg0;
+	x = (x & 0x00000000FFFFFFFF) << 32 | (x & 0xFFFFFFFF00000000) >> 32;
+	x = (x & 0x0000FFFF0000FFFF) << 16 | (x & 0xFFFF0000FFFF0000) >> 16;
+	x = (x & 0x00FF00FF00FF00FF) << 8  | (x & 0xFF00FF00FF00FF00) >> 8;
+return x;
+}
+
 local void nop(cpu *c) {}
 
 local void hlt(cpu *c) {
