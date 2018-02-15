363aee0483019ea35d7ac992d8fb972e6165884d
Johann150 <johann.galle@protonmail.com>
2018-02-15 20:14:42 +0100

    small mistake in size
    not i32 but i64

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 int main() {
     cpu *c = (cpu*)malloc(sizeof(cpu));
     cpu_init(c);
-    std::vector<byte> memory_vec{0x01, 0, 0, 0, 3, 0x02, 0, 0, 0, 4, 0x16, 0x2E};
+    std::vector<byte> memory_vec{0x01, 0, 0, 0, 0, 0, 0, 0, 3, 0x02, 0, 0, 0, 0, 0, 0, 0, 4, 0x16, 0x2E};
     c->mem = (byte*)malloc(memory_vec.size()*sizeof(byte));
     std::copy(memory_vec.begin(),memory_vec.end(),c->mem);
 
